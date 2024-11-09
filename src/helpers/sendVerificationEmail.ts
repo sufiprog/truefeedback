27cfeb5395a5187bf430d0f7dcb6ea5b454c5d8e
Muhammad Sufyan <122455815+sufiprog@users.noreply.github.com>
2024-11-09 15:17:29 +0000

    ,

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,3 +1,27 @@
 import {resend} from '@/lib/resend';
 import VerificationEmail from '../../emails/VerificationEmail';
+import { ApiResponse } from '@/types/ApiResponse';
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string
+):Promise<ApiResponse>{
+    try {
+
+        await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: email,
+            subject: 'Verification Code | True Feedback',
+            react: VerificationEmail({username, otp: verifyCode}),
+          });
+        
+        return {success: true, message: "Verification email send successfully"}
+
+        
+    } catch (emailError) {
+        console.log("Error sending verification email", emailError)
+        return {success: false, message: "Failed to send verification email"}
+    }
+}
 
