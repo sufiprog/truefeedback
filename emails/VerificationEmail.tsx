5a0279408b208da3d4cb938e24914fbcdba87e2c
sufiprog <sufidemo@gmail.com>
2024-11-09 19:44:48 +0500

    2nd commit

--- emails/VerificationEmail.tsx
+++ emails/VerificationEmail.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
     Html,
     Head,
@@ -8,13 +9,41 @@ import {
     Section,
     Text,
     Button,
-} from '@react-ewmail/componentssa';
+} from '@react-email/components';
 
 interface VerificationEmailProps {
     username: string;
     otp: string;
 }
 
-export default function VerificationEmail({ username, otp }: VerificationEmailProps){
-    
+export default function VerificationEmail({ username, otp }: VerificationEmailProps) {
+    return (
+        <Html lang="en" dir="ltr">
+            <Head>
+                <title>Verification Code</title>
+                <Font href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" />
+            </Head>
+            <Preview>Here's your verification code: {otp}</Preview>
+            <Section>
+                <Row>
+                    <Text>
+                        Hi {username},
+                    </Text>
+                </Row>
+                <Row>
+                    <Text>Thanks for Registering, Please use the following code</Text>
+                </Row>
+                <Row>
+                    <Text>
+                        {otp}
+                    </Text>
+                </Row>
+                <Row>
+                    <Text>
+                        This OTP will expire in 60 minutes.
+                    </Text>
+                </Row>
+            </Section>
+        </Html>
+    );
 }
